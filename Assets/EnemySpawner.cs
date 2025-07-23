70623c664d28e5c71d6b1c70d7e75b8211010812
titus <titustesche@outlook.de>
2025-07-23 22:39:58 +0200

    Subscribe to Time based events to make the game more difficult as it progresses

--- Assets/EnemySpawner.cs
+++ Assets/EnemySpawner.cs
@@ -1,12 +1,34 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] public GameObject enemyPrefab;
+    [SerializeField] public GameObject bossEnemyPrefab;
     [SerializeField] private float timeUntilSpawn;
     [SerializeField] private float minSpawnDelay;
     [SerializeField] private float maxSpawnDelay;
+    // private List<EnemyController> _childEnemies = new List<EnemyController>();
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private void Awake()
+    {
+        TimeManager.Instance.MinuteElapsed += DifficultyRamp;
+    }
+
+    // Basically just decrease the spawning delay every 5 minutes
+    void DifficultyRamp(int minutes)
+    {
+        if (minutes % 2 != 0) return;
+        maxSpawnDelay = maxSpawnDelay > 10 ? maxSpawnDelay - 5 : maxSpawnDelay;
+        if (minutes % 5 != 0) return;
+        // Have a 10% Chance of Spawning a boss enemy
+        if (Random.Range(1, 100) <= 10) Instantiate(bossEnemyPrefab, transform.position, Quaternion.identity);
+    }
+
     void Start()
     {
         timeUntilSpawn = Random.Range(minSpawnDelay, maxSpawnDelay);
@@ -20,7 +42,8 @@ public class EnemySpawner : MonoBehaviour
         if (timeUntilSpawn <= 0)
         {
             timeUntilSpawn = Random.Range(0, maxSpawnDelay);
-            Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+            var enemy = Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+            // _childEnemies.Add(enemy.GetComponent<EnemyController>());
         }
     }
 }
